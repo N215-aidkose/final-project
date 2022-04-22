c6a770036a78437b1f2f899408b4d194b5139b16
Aiden Kosegi <aidkose@iu.edu>
2022-04-22 15:19:52 -0400

    gallery menu and descriptions done

--- model/model.js
+++ model/model.js
@@ -121,11 +121,124 @@ var homeContent = `<div class="home">
   </div>
 </div>
 </div>`;
-var aboutContent = ``;
-var galleryContent = ``;
+var aboutContent = `      <div class="about">
+<div class="about-hero"></div>
+<div class="about-header">
+  <h1>OUR HISTORY:</h1>
+</div>
+<div class="about-text">
+  <p>
+    Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+    accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+    quae ab illo inventore veritatis et quasi architecto beatae vitae
+    dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+    aspernatur aut odit aut fugit, sed quia consequuntur
+  </p>
+</div>
+</div>`;
+var galleryContent = `      <div class="gallery">
+<h1>GALLERY:</h1>
+<div class="gallery-underline"></div>
+<div class="gallery-top">
+  <a href="#food" class="yellow-pic"></a>
+  <a href="#dj" class="pink-pic"></a>
+  <a href="#speech" class="green-pic"></a>
+</div>
+<div class="gallery-bottom">
+  <a class="purple-pic" href="#open"></a>
+  <a class="teal-pic" href="#international"></a>
+</div>
+</div>`;
 var blogContent = ``;
 var contactContent = ``;
-
+var foodContent = `<div class="food">
+<div class="food-hero"><h1>1. food festival</h1></div>
+<div class="gallery-description">
+  <div class="left-text">
+    Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+    accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+    quae ab illo inventore veritatis et quasi architecto beatae vitae
+    dicta sunt explicabo. Nemo
+  </div>
+  <div class="right-text">
+    Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
+    consectetur, adipisci velit, sed quia non numquam eius modi tempora
+    incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+  </div>
+</div>
+</div>`;
+var djContent = `      <div class="dj">
+<div class="dj-hero">
+  <h1>2. dee-jay</h1>
+</div>
+<div class="gallery-description">
+  <div class="left-text">
+    Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+    accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+    quae ab illo inventore veritatis et quasi architecto beatae vitae
+    dicta sunt explicabo. Nemo
+  </div>
+  <div class="right-text">
+    Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
+    consectetur, adipisci velit, sed quia non numquam eius modi tempora
+    incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+  </div>
+</div>
+</div>`;
+var speechContent = `      <div class="speech">
+<div class="speech-hero">
+  <h1>3. speech</h1>
+</div>
+<div class="gallery-description">
+  <div class="left-text">
+    Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+    accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+    quae ab illo inventore veritatis et quasi architecto beatae vitae
+    dicta sunt explicabo. Nemo
+  </div>
+  <div class="right-text">
+    Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
+    consectetur, adipisci velit, sed quia non numquam eius modi tempora
+    incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+  </div>
+</div>
+</div>`;
+var openContent = `      <div class="open">
+<div class="open-hero">
+  <h1>4. open foodfest</h1>
+</div>
+<div class="gallery-description">
+  <div class="left-text">
+    Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+    accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+    quae ab illo inventore veritatis et quasi architecto beatae vitae
+    dicta sunt explicabo. Nemo
+  </div>
+  <div class="right-text">
+    Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
+    consectetur, adipisci velit, sed quia non numquam eius modi tempora
+    incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+  </div>
+</div>
+</div>`;
+var internationalContent = `      <div class="international">
+<div class="international-hero">
+  <h1>5. international</h1>
+</div>
+<div class="gallery-description">
+  <div class="left-text">
+    Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+    accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+    quae ab illo inventore veritatis et quasi architecto beatae vitae
+    dicta sunt explicabo. Nemo
+  </div>
+  <div class="right-text">
+    Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
+    consectetur, adipisci velit, sed quia non numquam eius modi tempora
+    incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+  </div>
+</div>
+</div>`;
 // THIS FUNCTION IS BEING CALLED BY app.js which has the page name
 export function modelPageName(pgName) {
   console.log(pgName);
@@ -134,12 +247,12 @@ export function modelPageName(pgName) {
   // !make sure to use an operator "==" if checking
   if (pgName == "homeContent") {
     $("nav a").removeClass("black-text").addClass("white-text");
-    $("div").removeClass("black-underline");
-    $("div").addClass("white-underline");
+    $("div").removeClass("logo-black");
+    $("div").removeClass("black-underline").addClass("white-underline");
   } else {
     $("nav a").removeClass("white-text").addClass("black-text");
-    $("div").removeClass("white-underline");
-    $("div").addClass("black-underline");
+    $("div").removeClass("logo-white");
+    $("div").removeClass("white-underline").addClass("black-underline");
   }
   try {
     eval(pgName);
